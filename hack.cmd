e1fc371488136511a8f3d90416f8cb72d73c0050
Unknown <alexander.entin@gmail.com>
2019-04-08 23:07:27 +0300

    mvp

---
+++ hack.cmd
@@ -0,0 +1,1 @@
+start "phonegap" phonegap serve
---
+++ www/index.js
@@ -0,0 +1,8 @@
+function go() {
+    let url = document.getElementById('url').value
+    if (url.toLowerCase().indexOf('http') !== 0) {
+        url = 'http://' + url
+    }
+    // location.href = url
+    document.getElementById('app').src = url
+}
