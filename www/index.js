25100a4e611bdc02a6a4e09f7bfaec467a23c45e
Alexander Entin <alexander.entin@gmail.com>
2019-04-16 07:48:39 +0300

    fixed protocol detection

--- www/index.js
+++ www/index.js
@@ -1,6 +1,6 @@
 function go() {
     let url = document.getElementById('url').value
-    if (url.toLowerCase().indexOf('://') !== 0) {
+    if (url.toLowerCase().indexOf('://') === -1) {
         url = 'http://' + url
     }
     location.href = url
